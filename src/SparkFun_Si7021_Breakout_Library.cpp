01dea9533019187ac5422b9570d1cc8af894aa82
Nathan Seidle <nathan@sparkfun.com>
2023-11-28 02:22:27 -0700

    Whitespace format

--- src/SparkFun_Si7021_Breakout_Library.cpp
+++ src/SparkFun_Si7021_Breakout_Library.cpp
@@ -1,11 +1,11 @@
 /*
- SparkFun Si7021 Temperature and HUmidity Breakout 
+ SparkFun Si7021 Temperature and HUmidity Breakout
  By: Joel Bartlett
  SparkFun Electronics
  Date: December 10, 2015
- 
+
  This is an Arduino library for the Si7021 Temperature and Humidity Sensor Breakout
- 
+
  This library is based on the following libraries:
 
  HTU21D Temperature / Humidity Sensor Library
@@ -31,205 +31,206 @@
  */
 
 #if defined(ARDUINO)
-#include "Wire.h"
 #include "SparkFun_Si7021_Breakout_Library.h"
+#include "Wire.h"
 #elif defined(SPARK)
 #include "SparkFun_Si7021_Breakout_Library/SparkFun_Si7021_Breakout_Library.h"
 #endif
 
-
- //Initialize
- Weather::Weather(){}
-
- bool Weather::begin()
+// Initialize
+Weather::Weather()
 {
-  Wire.begin();
+}
 
-  uint8_t ID_Temp_Hum = checkID();
+bool Weather::begin()
+{
+    Wire.begin();
 
-  int x = 0;
+    uint8_t ID_Temp_Hum = checkID();
 
-  if(ID_Temp_Hum == 0x15)//Ping CheckID register
-    x = 1;
-  else if(ID_Temp_Hum == 0x32)
-    x = 2;
-  else
-    x = 0;
+    int x = 0;
 
-  if(x == 1)
-  {
-    Serial.println("Si7021 Found");
-    //Serial.println(ID_Temp_Hum, HEX);
-    return true;
-  }
-  else if(x == 2)
-  {
-    Serial.println("HTU21D Found");
-    //Serial.println(ID_Temp_Hum, HEX);
-    return true;
-  }
-  else
-  	Serial.println("No Devices Detected");
-  	//Serial.println(ID_Temp_Hum, HEX);
-	 return false;
+    if (ID_Temp_Hum == 0x15) // Ping CheckID register
+        x = 1;
+    else if (ID_Temp_Hum == 0x32)
+        x = 2;
+    else
+        x = 0;
+
+    if (x == 1)
+    {
+        Serial.println("Si7021 Found");
+        // Serial.println(ID_Temp_Hum, HEX);
+        return true;
+    }
+    else if (x == 2)
+    {
+        Serial.println("HTU21D Found");
+        // Serial.println(ID_Temp_Hum, HEX);
+        return true;
+    }
+    else
+        Serial.println("No Devices Detected");
+    // Serial.println(ID_Temp_Hum, HEX);
+    return false;
 }
 
 /****************Si7021 & HTU21D Functions**************************************/
 
-
 float Weather::getRH()
 {
-	// Measure the relative humidity
-	uint16_t RH_Code = makeMeasurment(HUMD_MEASURE_NOHOLD);
-	float result = (125.0*RH_Code/65536)-6;
-	return result;
+    // Measure the relative humidity
+    uint16_t RH_Code = makeMeasurment(HUMD_MEASURE_NOHOLD);
+    float result = (125.0 * RH_Code / 65536) - 6;
+    return result;
 }
 
 float Weather::readTemp()
 {
-	// Read temperature from previous RH measurement.
-	uint16_t temp_Code = makeMeasurment(TEMP_PREV);
-	float result = (175.72*temp_Code/65536)-46.85;
-	return result;
+    // Read temperature from previous RH measurement.
+    uint16_t temp_Code = makeMeasurment(TEMP_PREV);
+    float result = (175.72 * temp_Code / 65536) - 46.85;
+    return result;
 }
 
 float Weather::getTemp()
 {
-	// Measure temperature
-	uint16_t temp_Code = makeMeasurment(TEMP_MEASURE_NOHOLD);
-	float result = (175.72*temp_Code/65536)-46.85;
-	return result;
+    // Measure temperature
+    uint16_t temp_Code = makeMeasurment(TEMP_MEASURE_NOHOLD);
+    float result = (175.72 * temp_Code / 65536) - 46.85;
+    return result;
 }
-//Give me temperature in fahrenheit!
+// Give me temperature in fahrenheit!
 float Weather::readTempF()
 {
-  return((readTemp() * 1.8) + 32.0); // Convert celsius to fahrenheit
+    return ((readTemp() * 1.8) + 32.0); // Convert celsius to fahrenheit
 }
 
 float Weather::getTempF()
 {
-  return((getTemp() * 1.8) + 32.0); // Convert celsius to fahrenheit
+    return ((getTemp() * 1.8) + 32.0); // Convert celsius to fahrenheit
 }
-
 
 void Weather::heaterOn()
 {
-	// Turns on the ADDRESS heater
-	uint8_t regVal = readReg();
-	regVal |= _BV(HTRE);
-	//turn on the heater
-	writeReg(regVal);
+    // Turns on the ADDRESS heater
+    uint8_t regVal = readReg();
+    regVal |= _BV(HTRE);
+    // turn on the heater
+    writeReg(regVal);
 }
 
 void Weather::heaterOff()
 {
-	// Turns off the ADDRESS heater
-	uint8_t regVal = readReg();
-	regVal &= ~_BV(HTRE);
-	writeReg(regVal);
+    // Turns off the ADDRESS heater
+    uint8_t regVal = readReg();
+    regVal &= ~_BV(HTRE);
+    writeReg(regVal);
 }
 
 void Weather::changeResolution(uint8_t i)
 {
-	// Changes to resolution of ADDRESS measurements.
-	// Set i to:
-	//      RH         Temp
-	// 0: 12 bit       14 bit (default)
-	// 1:  8 bit       12 bit
-	// 2: 10 bit       13 bit
-	// 3: 11 bit       11 bit
+    // Changes to resolution of ADDRESS measurements.
+    // Set i to:
+    //      RH         Temp
+    // 0: 12 bit       14 bit (default)
+    // 1:  8 bit       12 bit
+    // 2: 10 bit       13 bit
+    // 3: 11 bit       11 bit
 
-	uint8_t regVal = readReg();
-	// zero resolution bits
-	regVal &= 0b011111110;
-	switch (i) {
-	  case 1:
-	    regVal |= 0b00000001;
-	    break;
-	  case 2:
-	    regVal |= 0b10000000;
-	    break;
-	  case 3:
-	    regVal |= 0b10000001;
-	  default:
-	    regVal |= 0b00000000;
-	    break;
-	}
-	// write new resolution settings to the register
-	writeReg(regVal);
+    uint8_t regVal = readReg();
+    // zero resolution bits
+    regVal &= 0b011111110;
+    switch (i)
+    {
+    case 1:
+        regVal |= 0b00000001;
+        break;
+    case 2:
+        regVal |= 0b10000000;
+        break;
+    case 3:
+        regVal |= 0b10000001;
+    default:
+        regVal |= 0b00000000;
+        break;
+    }
+    // write new resolution settings to the register
+    writeReg(regVal);
 }
 
 void Weather::reset()
 {
-	//Reset user resister
-	writeReg(SOFT_RESET);
+    // Reset user resister
+    writeReg(SOFT_RESET);
 }
 
 uint8_t Weather::checkID()
 {
-	uint8_t ID_1;
+    uint8_t ID_1;
 
- 	// Check device ID
-	Wire.beginTransmission(ADDRESS);
-	Wire.write(0xFC);
-	Wire.write(0xC9);
-	Wire.endTransmission();
+    // Check device ID
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0xFC);
+    Wire.write(0xC9);
+    Wire.endTransmission();
 
-    Wire.requestFrom(ADDRESS,1);
+    Wire.requestFrom(ADDRESS, 1);
 
     ID_1 = Wire.read();
 
-    return(ID_1);
+    return (ID_1);
 }
 
 uint16_t Weather::makeMeasurment(uint8_t command)
 {
-	// Take one ADDRESS measurement given by command.
-	// It can be either temperature or relative humidity
-	// TODO: implement checksum checking
+    // Take one ADDRESS measurement given by command.
+    // It can be either temperature or relative humidity
+    // TODO: implement checksum checking
 
-	uint16_t nBytes = 3;
-	// if we are only reading old temperature, read olny msb and lsb
-	if (command == 0xE0) nBytes = 2;
+    uint16_t nBytes = 3;
+    // if we are only reading old temperature, read olny msb and lsb
+    if (command == 0xE0)
+        nBytes = 2;
 
-	Wire.beginTransmission(ADDRESS);
-	Wire.write(command);
-	Wire.endTransmission();
-	// When not using clock stretching (*_NOHOLD commands) delay here
-	// is needed to wait for the measurement.
-	// According to datasheet the max. conversion time is ~22ms
-	 delay(100);
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(command);
+    Wire.endTransmission();
+    // When not using clock stretching (*_NOHOLD commands) delay here
+    // is needed to wait for the measurement.
+    // According to datasheet the max. conversion time is ~22ms
+    delay(100);
 
-	Wire.requestFrom(ADDRESS,nBytes);
-	if(Wire.available() != nBytes)
-  	return 100;
-	
-	unsigned int msb = Wire.read();
-	unsigned int lsb = Wire.read();
-	// Clear the last to bits of LSB to 00.
-	// According to datasheet LSB of RH is always xxxxxx10
-	lsb &= 0xFC;
-	unsigned int mesurment = msb << 8 | lsb;
+    Wire.requestFrom(ADDRESS, nBytes);
+    if (Wire.available() != nBytes)
+        return 100;
 
-	return mesurment;
+    unsigned int msb = Wire.read();
+    unsigned int lsb = Wire.read();
+    // Clear the last to bits of LSB to 00.
+    // According to datasheet LSB of RH is always xxxxxx10
+    lsb &= 0xFC;
+    unsigned int mesurment = msb << 8 | lsb;
+
+    return mesurment;
 }
 
 void Weather::writeReg(uint8_t value)
 {
-	// Write to user register on ADDRESS
-	Wire.beginTransmission(ADDRESS);
-	Wire.write(WRITE_USER_REG);
-	Wire.write(value);
-	Wire.endTransmission();
+    // Write to user register on ADDRESS
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(WRITE_USER_REG);
+    Wire.write(value);
+    Wire.endTransmission();
 }
 
 uint8_t Weather::readReg()
 {
-	// Read from user register on ADDRESS
-	Wire.beginTransmission(ADDRESS);
-	Wire.write(READ_USER_REG);
-	Wire.endTransmission();
-	Wire.requestFrom(ADDRESS,1);
-	uint8_t regVal = Wire.read();
-	return regVal;
+    // Read from user register on ADDRESS
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(READ_USER_REG);
+    Wire.endTransmission();
+    Wire.requestFrom(ADDRESS, 1);
+    uint8_t regVal = Wire.read();
+    return regVal;
 }
--- src/SparkFun_Si7021_Breakout_Library.h
+++ src/SparkFun_Si7021_Breakout_Library.h
@@ -1,11 +1,11 @@
 /*
- SparkFun Si7021 Temperature and Humidity Breakout 
+ SparkFun Si7021 Temperature and Humidity Breakout
  By: Joel Bartlett
  SparkFun Electronics
  Date: December 10, 2015
- 
+
  This is an Arduino library for the Si7021 Temperature and Humidity Sensor Breakout
- 
+
  This library is based on the following libraries:
 
  HTU21D Temperature / Humidity Sensor Library
@@ -37,56 +37,53 @@
 
 /****************Si7021 & HTU21D Definitions***************************/
 
-#define ADDRESS      0x40
+#define ADDRESS 0x40
 
-#define TEMP_MEASURE_HOLD  0xE3
-#define HUMD_MEASURE_HOLD  0xE5
-#define TEMP_MEASURE_NOHOLD  0xF3
-#define HUMD_MEASURE_NOHOLD  0xF5
-#define TEMP_PREV   0xE0
+#define TEMP_MEASURE_HOLD 0xE3
+#define HUMD_MEASURE_HOLD 0xE5
+#define TEMP_MEASURE_NOHOLD 0xF3
+#define HUMD_MEASURE_NOHOLD 0xF5
+#define TEMP_PREV 0xE0
 
-#define WRITE_USER_REG  0xE6
-#define READ_USER_REG  0xE7
-#define SOFT_RESET  0xFE
+#define WRITE_USER_REG 0xE6
+#define READ_USER_REG 0xE7
+#define SOFT_RESET 0xFE
 
-#define HTRE        0x02
+#define HTRE 0x02
 #define _BV(bit) (1 << (bit))
 
 #define CRC_POLY 0x988000 // Shifted Polynomial for CRC check
 
 // Error codes
-#define I2C_TIMEOUT 	998
-#define BAD_CRC		999
-
+#define I2C_TIMEOUT 998
+#define BAD_CRC 999
 
 /****************Si7021 & HTU21D Class**************************************/
 class Weather
 {
-public:
-	// Constructor
-	Weather();
+  public:
+    // Constructor
+    Weather();
 
-	bool  begin();
+    bool begin();
 
-	// Si7021 & HTU21D Public Functions
-	float getRH();
-	float readTemp();
-	float getTemp();
-	float readTempF();
-	float getTempF();
-	void  heaterOn();
-	void  heaterOff();
-	void  changeResolution(uint8_t i);
-	void  reset();
-	uint8_t  checkID();
+    // Si7021 & HTU21D Public Functions
+    float getRH();
+    float readTemp();
+    float getTemp();
+    float readTempF();
+    float getTempF();
+    void heaterOn();
+    void heaterOff();
+    void changeResolution(uint8_t i);
+    void reset();
+    uint8_t checkID();
 
-
-
-private:
-	//Si7021 & HTU21D Private Functions
-	uint16_t makeMeasurment(uint8_t command);
-	void     writeReg(uint8_t value);
-	uint8_t  readReg();
+  private:
+    // Si7021 & HTU21D Private Functions
+    uint16_t makeMeasurment(uint8_t command);
+    void writeReg(uint8_t value);
+    uint8_t readReg();
 };
 
 #endif
