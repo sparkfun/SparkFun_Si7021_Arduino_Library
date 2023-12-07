3311aa3b85684719ae2720b4eb78eb722f321018
Nathan Seidle <nathan@sparkfun.com>
2023-12-06 20:25:27 -0700

    Update main example

--- examples/SparkFun_Si7021_Breakout_Example/SparkFun_Si7021_Breakout_Example.ino
+++ examples/SparkFun_Si7021_Breakout_Example/SparkFun_Si7021_Breakout_Example.ino
@@ -3,10 +3,10 @@
   Joel Bartlett @ SparkFun Electronics
   Original Creation Date: May 18, 2015
   Updated November 28th, 2023
-  This sketch prints the temperature and humidity the Serial port.
+  This sketch prints the temperature and humidity to the Serial port.
 
   Hardware Connections:
-      HTU21D ------------- Arduino
+      Si7021 ------------- Arduino
       (-) ------------------- GND
       (+) ------------------- 3.3V (VCC)
        CL ------------------- SCL
@@ -21,7 +21,7 @@
 #include "SparkFun_Si7021_Breakout_Library.h" //http://librarymanager/All#SparkFun_Si7021
 #include <Wire.h>
 
-Weather myHumidity;
+SI7021 myHumidity;
 
 void setup()
 {
@@ -34,27 +34,53 @@ void setup()
     while (true);
   }
   Serial.println("Humidity sensor detected");
+
+  myHumidity.setResolution(0); // RH 12-bit, Temp 14-bit (Default)
+  //myHumidity.setResolution(1); // RH 8-bit, Temp 12-bit (Default)
+  //myHumidity.setResolution(2); // RH 10-bit, Temp 13-bit (Default)
+  //myHumidity.setResolution(3); // RH 11-bit, Temp 11-bit (Default)
+  int resolution = myHumidity.getResolution();
+
+  resolution *= 2;
+
+  myHumidity.heaterOn(); //Turn internal heater on
+  //myHumidity.heaterOff(); //Turn heater off
+  //myHumidity.setHeater(true); //Another way to turn heater on
+  bool isHeaterOn = myHumidity.getHeater();
+
+  if(isHeaterOn) Serial.println("Heater on");
+
+  myHumidity.setHeaterCurrent(0b0000); //Set heater to min 3.09mA (Default)
+  //myHumidity.setHeaterCurrent(0b1111); //Set heater to max 94.2mA
+  int heaterCurrent = myHumidity.getHeaterCurrent();
+
+  heaterCurrent *= 2;
 }
 
 void loop()
 {
-  // Measure Relative Humidity from the HTU21D or Si7021
+  // Measure Relative Humidity from the Si7021
   float humidity = myHumidity.getRH();
 
-  // Measure Temperature from the HTU21D or Si7021
-  float tempf = myHumidity.getTempF();
+  // Measure Temperature from the Si7021
+  float tempf = myHumidity.getTemperatureF();
 
   // Temperature is measured every time RH is requested.
-  // It is faster, therefore, to read it from previous RH
-  // measurement with getTemp() instead with readTemp()
+  // You can also read the previous reading with getPreviousTemperature()
 
-  Serial.print("Temp:");
+  Serial.print("Temperature:");
   Serial.print(tempf, 2);
   Serial.print("F, ");
 
   Serial.print("Humidity:");
   Serial.print(humidity, 1);
-  Serial.println("%");
+  Serial.print("%, ");
+
+  uint64_t deviceSerialNumber = myHumidity.getSerialNumber();
+  Serial.print("Serial Number: ");
+  Serial.print(deviceSerialNumber);
+
+  Serial.println();
 
   delay(1000);
 }
